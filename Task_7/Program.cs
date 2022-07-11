88207e70e52f30d24631fa19f84cd34a9f1dd307
Dasha <temnikova1010@gmail.com>
2022-07-12 03:09:37 +0700

    Add to file Task_7 finding last digit

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+
+int num = Convert.ToInt32 (Console.ReadLine());
+
+if (1000 > num)
+{
+    if (num > 99)
+    {
+    int lastDigit = num % 10;
+    Console.Write(lastDigit);
+    }
+    else
+    {
+    Console.WriteLine("Ошибка! Введите корректное число");
+    }
+}
+else
+{
+Console.WriteLine("Ошибка! Введите корректное число");
+}
