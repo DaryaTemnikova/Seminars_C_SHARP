691b3030a62e9ac7e133b9eea0901dd9d649f69b
Dasha <temnikova1010@gmail.com>
2022-07-15 21:44:37 +0700

    Add Task 11 delete second digit

---
+++ Task_11/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+int num = new Random().Next(100, 1000);
+
+int DeleteSecond(int number)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+
+int digit = DeleteSecond(num);
+Console.WriteLine($"{num} -> {digit}.");
