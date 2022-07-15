e77368700b64a0db3a2903f6d7639d7ba4add69f
Dasha <temnikova1010@gmail.com>
2022-07-15 22:30:05 +0700

    Add Task 14 multiplicity to 7 and 23

---
+++ Task_14/Program.cs
@@ -0,0 +1,26 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+int MultDigit(int number)
+{
+    int result1 = number % 7;
+    int result2 = number % 23;
+    
+    if (result1 == 0 & result2 == 0) return 0;
+    else return -1;
+}
+
+
+int multDigit = MultDigit(num);
+if(multDigit == 0) Console.WriteLine($"{num} -> да");
+else Console.WriteLine($"{num} -> нет");
+    
