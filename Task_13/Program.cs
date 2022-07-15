d653bcbe570ee5c2b2a968711183fba72d5c915b
Dasha <temnikova1010@gmail.com>
2022-07-16 00:11:36 +0700

    Add Task 13 finding third Digit

---
+++ Task_13/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру 
+// заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+int ThirdDigit(int number)
+{
+    int index = 0;
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int tirdDigit = number % 10;
+    
+    if(number < 100) return -1;
+    else return tirdDigit;
+}
+
+int digit = ThirdDigit(num);
+
+if(digit == -1)
+{
+    Console.WriteLine($"{num} -> третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"{num} -> {digit}.");
+}
